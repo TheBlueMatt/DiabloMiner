7145a48c9218b6f8cc6be099057ad9c937bb8f96
Matt Corallo <matt@bluematt.me>
2011-04-29 13:53:39 +0200

    Loop length can now be configured with -z
    Conflicts:
    
    	src/main/java/com/diablominer/DiabloMiner/DiabloMiner.java

--- src/main/java/com/diablominer/DiabloMiner/DiabloMiner.java
+++ src/main/java/com/diablominer/DiabloMiner/DiabloMiner.java
@@ -75,6 +75,7 @@ class DiabloMiner {
   int forceWorkSize = 64;
   boolean debug = false;
   int getworkRefresh = 5000;
+  int zloops = 0;
 
   String source;
 
@@ -121,6 +122,7 @@ class DiabloMiner {
     options.addOption("D", "devices", true, "devices to enable");
     options.addOption("x", "proxy", true, "optional proxy settings IP:PORT<:username:password>");
     options.addOption("l", "url", true, "bitcoin host url");
+    options.addOption("z", "loops", true, "kernel loops (power of two, 0 is off)");
     options.addOption("d", "debug", false, "enable extra debug output");
     options.addOption("h", "help", false, "this help");
 
@@ -170,6 +172,9 @@ class DiabloMiner {
 
     if(line.hasOption("port"))
       port = line.getOptionValue("port");
+
+    if(line.hasOption("loops"))
+      zloops = (int) Math.pow(2, Integer.parseInt(line.getOptionValue("loops")));
 
     if(line.hasOption("devices")){
       String devices[] = line.getOptionValue("devices").split(",");
@@ -349,6 +354,7 @@ class DiabloMiner {
 
     long workSize;
     long workSizeBase;
+
     final PointerBuffer localWorkSize = BufferUtils.createPointerBuffer(1);
 
     final ExecutionState executions[] = new ExecutionState[EXECUTION_TOTAL];;
@@ -386,13 +392,20 @@ class DiabloMiner {
         loops = LOOPS;
       }
 
+      if(zloops > 1)
+        loops = zloops;
+      else if(zloops == 1)
+        loops = 1;
+
       String compileOptions = "";
 
       if(hasBitAlign)
         compileOptions += " -D BITALIGN";
 
-      if(loops > 1)
+      if(loops > 1) {
         compileOptions += " -D DOLOOPS";
+        compileOptions += " -D LOOPS=" + loops;
+      }
 
       if(forceWorkSize > 0)
         compileOptions += " -D WORKGROUPSIZE=" + forceWorkSize;
@@ -434,7 +447,7 @@ class DiabloMiner {
 
       info("Added " + deviceName + " (" + deviceCU + " CU, local work size of " + localWorkSize.get(0) + ")");
 
-      workSizeBase = localWorkSize.get(0) * ((LOOPS + 1) - loops);
+      workSizeBase = (long) (localWorkSize.get(0) * ((float)LOOPS / (float)loops) * deviceCU);
       workSize = workSizeBase;
 
       for(int i = 0; i < EXECUTION_TOTAL; i++) {
